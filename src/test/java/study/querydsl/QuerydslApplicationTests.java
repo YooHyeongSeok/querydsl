c5c61b7d5b0c00ac42c62290d9d555123a4bde37
denver <denver@gccompany.co.kr>
2021-11-05 15:13:59 +0900

    case 문

--- src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -2,6 +2,8 @@ package study.querydsl;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
+import com.querydsl.core.types.dsl.CaseBuilder;
+import com.querydsl.core.types.dsl.NumberExpression;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.BeforeEach;
@@ -549,7 +551,59 @@ class QuerydslApplicationTests {
 					tuple.get(JPAExpressions.select(memberSub.age.avg()) .from(memberSub)));
 		}
 
+	}
 
+	@Test
+	public void selectCase() {
+
+		List<String> result =
+				queryFactory
+					.select(member.age
+						.when(10).then("열살")
+						.when(20).then("스무살")
+						.otherwise("기타")
+					)
+					.from(member) .fetch();
+
+	}
+
+	@Test
+	public void selectComplexCase() {
+
+		List<String> result =
+				queryFactory
+						.select(
+								new CaseBuilder()
+								.when(member.age.between(0, 20)).then("0~20살")
+								.when(member.age.between(21, 30)).then("21~30살")
+								.otherwise("기타")
+						)
+						.from(member)
+						.fetch();
+
+	}
+
+	@Test
+	public void useCaseWithOrderBy() {
+		NumberExpression<Integer> rankPath =
+				new CaseBuilder()
+						.when(member.age.between(0, 20)).then(2)
+						.when(member.age.between(21, 30)).then(1)
+						.otherwise(3);
+
+		List<Tuple> result =
+				queryFactory
+						.select(member.username, member.age, rankPath)
+						.from(member)
+						.orderBy(rankPath.desc())
+						.fetch();
+
+		for (Tuple tuple : result) {
+			String username = tuple.get(member.username); Integer age = tuple.get(member.age);
+
+			Integer rank = tuple.get(rankPath);
+			System.out.println("username = " + username + " age = " + age + " rank = " + rank);
+		}
 	}
 
 }
